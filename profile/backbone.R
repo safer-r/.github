fa8c3db2fcc96c2116baf9d57b21fb37e827b93b
gmillot <gael.millot@pasteur.fr>
2024-12-27 15:03:40 +0100

    release v4.5: implementation in BACKBONE for .base_op_check

--- profile/backbone.R
+++ profile/backbone.R
@@ -79,7 +79,7 @@ BACKBONE <- function(data, lib_path = NULL, seed = NULL, safer_check = TRUE, err
     }
     if(safer_check == TRUE){
         saferDev:::.base_op_check(
-            error_text = error_text
+            error_text = base::sub(pattern = "^ERROR IN ", replacement = " INSIDE ", x = error_text_start, ignore.case = FALSE, perl = FALSE, fixed = FALSE, useBytes = FALSE)
         )
     }
     #### end critical operator checking
@@ -124,7 +124,7 @@ BACKBONE <- function(data, lib_path = NULL, seed = NULL, safer_check = TRUE, err
             ),
             lib_path = lib_path, # write NULL if your function does not have any lib_path argument
             safer_check = FALSE, # TRUE only when .pack_and_function_check() is not used inside safer function
-            error_text = error_text, 
+            error_text = base::paste0(error_text_start, error_text, collapse = NULL, recycle0 = FALSE), 
             internal_error_report_link = internal_error_report_link
         )
     }
